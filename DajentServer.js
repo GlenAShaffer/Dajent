91357012bf411fedfec21f441d73af6d7e91dd5b
Glen Shaffer <glenashaffer@gmail.com>
2025-08-16 20:19:22 -0400

    Created a ServerFunctions.js module in the bin folder, and added newCodeVerifier and generateCodeChallenge functions.

---
+++ DajentServer.js
@@ -0,0 +1,8 @@
+//DajentServer.js
+
+//Imports
+//Remember that generateCodeChallenge is async.
+import {newCodeVerifier, generateCodeChallenge} from './bin/ServerFunctions.js';
+
+//Script
+console.log("Running");
---
+++ bin/ServerFunctions.js
@@ -0,0 +1,22 @@
+//ServerFunctions.js
+
+//A function for creating a code verifier
+//The code verifier must be 43 to 128 characters long, and contain letters, digits, underscores, periods, hyphens, tildes.
+export function newCodeVerifier() {
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const values = crypto.getRandomValues(new Uint8Array(128));
+    return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+};
+
+
+//A function for generating a code challenge
+//This function should take a code verifier, hash it using SHA256, and then encode it in base64.
+//Remember that crypto.subtle.digest is async, and that this function is async.
+export async function generateCodeChallenge(code_verifier) {
+    const encoder = new TextEncoder();
+    const encoded_string = encoder.encode(code_verifier);
+    console.log("Encoded text: " + encoded_string);
+    const hash = await crypto.subtle.digest('SHA-256', encoded_string);
+    console.log("Hash: " + hash);
+    return btoa(String.fromCharCode(... new Uint8Array(hash))).replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_');
+};
