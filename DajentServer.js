3cfdbb96d4d31e9292b599885674367de34b812e
Glen Shaffer <glenashaffer@gmail.com>
2025-08-17 15:36:00 -0400

    Added formatted strings to and a PORT variable to DajentServer.js so that the port is configurable.

--- DajentServer.js
+++ DajentServer.js
@@ -5,13 +5,16 @@
 import {newCodeVerifier, generateCodeChallenge, openBrowser} from './bin/ServerFunctions.js';
 import { createServer } from 'node:http';
 
-//Script
-console.log("");
+//Variables
+const PORT = 8080;
 
+//Script
 createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('Hello World! This is Dajent.');
   res.end();
 }).listen(8080);
 
-openBrowser('http://localhost:8080');
+console.log(`Dajent Server listening on port ${PORT}.`);
+
+openBrowser(`http://localhost:${PORT}`);
